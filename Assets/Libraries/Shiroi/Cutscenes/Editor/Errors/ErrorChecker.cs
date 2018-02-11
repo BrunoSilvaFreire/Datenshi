253e3e8eda022d05cbaae0fe59f98ef9d1b35da6
DDevilISL <brunorbsf@gmail.com>
2018-02-11 21:25:09 -0200

    - Added ShiroiCutscenes, ReWired, DOTween and Odin as libraries

---
+++ Assets/Libraries/Shiroi/Cutscenes/Editor/Errors/ErrorChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Shiroi.Cutscenes.Tokens;
+
+namespace Shiroi.Cutscenes.Editor.Errors {
+    public abstract class ErrorChecker {
+        public abstract void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token,
+            object value, int fieldIndex, FieldInfo info);
+    }
+
+    public interface IOnBeginCheckListener {
+        void OnBegin(ErrorManager manager, CutsceneEditor editor);
+    }
+    
+    public interface IOnEndCheckListener {
+        void OnEnd(ErrorManager manager, CutsceneEditor editor);
+    }
+}
---
+++ Assets/Libraries/Shiroi/Cutscenes/Util/Reference.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Shiroi.Cutscenes.Util {
+    public class Reference {
+        public enum ReferenceType : byte{
+            Future = 0,
+            Exposed = 1
+        }
+
+        public ReferenceType Type;
+        public int Id;
+
+        public Reference(ReferenceType type, int id)  {
+            Type = type;
+            Id = id;
+        }
+        public Object Resolve(CutscenePlayer player) {
+            switch (Type) {
+                case ReferenceType.Exposed:
+                    bool idValid;
+                    Object referenceValue = player.GetReferenceValue(PropertyName, out idValid);
+                    if (idValid) { 
+                        return referenceValue;
+                    } else { 
+                        return null;
+                    }
+                case ReferenceType.Future:
+                    return player.RequestFuture(Id);
+            }
+            return null;
+        }
+
+        public PropertyName PropertyName {
+            get {
+                return new PropertyName(Id); 
+            } 
+        }
+    }
+    
+    public sealed class Reference<T> : Reference where T : Object{
+        public Reference(ReferenceType type, int id) : base(type, id) { }
+
+        public T Resolve(CutscenePlayer player) {
+            switch (Type) {
+                case ReferenceType.Exposed:
+                    bool idValid;
+                    Object referenceValue = player.GetReferenceValue(PropertyName, out idValid);
+                    if (idValid) { 
+                        return referenceValue as T;
+                    } else { 
+                        return null;
+                    }
+                case ReferenceType.Future:
+                    return player.RequestFuture<T>(Id);
+            }
+            return null;
+        }
+
+
+        public static implicit operator ExposedReference<T>(Reference<T> reference) {
+            var refe = new ExposedReference<T>();
+            refe.exposedName = reference.PropertyName;
+            return refe;
+        }
+
+    }
+}
